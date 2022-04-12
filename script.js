585542156ed64f769046490ecb288db18da36887
Bakary Sylla <syllabakary2002@gmail.com>
2022-04-12 14:59:23 -0700

    WIP

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//<a class="waves-effect waves-light btn-large" href="#">Wave</a>
 
 //var repoList = document.querySelector('ul');
 var fetchButton = document.getElementById('fetch-button');
@@ -12,14 +11,31 @@ function getiIngredientsList(event){
   event.preventDefault();
   var searchInputText = document.getElementById('ingredients-input').value.trim();
  console.log(searchInputText);
-fetch(`https://api.spoonacular.com/recipes/716429/information?apiKey=414afb9d3ee94e3db1c1540d721fe959&ingredients=${searchInputText}`)
+ //api doesn't work properly
+fetch(`https://api.spoonacular.com/recipes/information?apiKey=0982c029a4d04bb5b43ac973cac0a1bd&ingredients=${searchInputText}`)
 .then(function (response) {
   return response.json();
 })
 .then(function (data) {
   console.log(data)
+
+  // for (var i = 0; i < data.length; i++) {
+    //Creating a h3 element and a p element
+    var userName = document.createElement('h3');
+    var userUrl = document.createElement('img');
+
+    //Setting the text of the h3 element and p element.
+    userName.textContent = data.title;
+    userUrl.src = data.image;
+
+    //Appending the dynamically generated html to the div associated with the id="users"
+    //Append will attach the element as the bottom most child.
+    document.getElementById('link').append(userName, userUrl)
+    
+  // }
 });
 }
+
 //getiIngredientsList();
 
 
