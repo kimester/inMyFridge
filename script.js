e11c46c079ee0d7146519a3f27338af3586ae535
Surbhi Arora <arora.surbhi4@gmail.com>
2022-04-13 10:25:57 -0700

    changes

--- script.js
+++ script.js
@@ -11,15 +11,16 @@ $(document).ready(function(){
 
     $("#recipes").submit(function(event){
         event.preventDefault()
-        alert("form is submitted")
+        //alert("form is submitted")
         var search = $("#search").val()
-
-        videoSearch(YT_API_KEY, search, 4)
+    
+      videoSearch(YT_API_KEY, search, 4)
+        getiIngredientsList(search)
     });
 
     function videoSearch(key,search, maxResults) {
 
-        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults" + maxResults + "&q=" + search, function(data){
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults" + maxResults + "&q=" + search + "recipes", function(data){
             console.log(data)
 
             data.items.forEach(item =>  {
@@ -48,33 +49,47 @@ $(document).ready(function(){
 // console.log(ingredientsList)
 // console.log(fetchButton)
 
-fetchButton.addEventListener('click', getiIngredientsList);
+// fetchButton.addEventListener('click', getiIngredientsList);
 
-function getiIngredientsList(event){
-  event.preventDefault();
-  var searchInputText = document.getElementById('ingredients-input').value.trim();
- console.log(searchInputText);
+// function getiIngredientsList(event){
+//   event.preventDefault();
+//   var searchInputText = document.getElementById('ingredients-input').value.trim();
+//  console.log(searchInputText);
  //api doesn't work properly
-fetch(`https://api.spoonacular.com/recipes/information?apiKey=0982c029a4d04bb5b43ac973cac0a1bd&ingredients=${searchInputText}`)
+
+ function getiIngredientsList(searchInputText)
+ { //SA: Testing this change , otherwise delete this line and uncomment from 51-56
+fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=0982c029a4d04bb5b43ac973cac0a1bd&ingredients=${searchInputText}&number=2`)
 .then(function (response) {
   return response.json();
 })
 .then(function (data) {
   console.log(data)
 
-  // for (var i = 0; i < data.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     //Creating a h3 element and a p element
     var userName = document.createElement('h3');
     var userUrl = document.createElement('img');
+  
 
     //Setting the text of the h3 element and p element.
-    userName.textContent = data.title;
-    userUrl.src = data.image;
+    userName.textContent = data[i].title;
+    userUrl.src = data[i].image;
 
+   
+  // var ingredients =data[i].usedIngredients[0];
+            // for(i=0;i<data[i].usedIngredients[0].length;i++)
+            // {
+            //   var recipeList = document.createElement('h3');
+            //   recipeList.textContent=data[i].usedIngredients[0][i];
+            //   document.getElementById('recipe').append(recipeList)
+            // }
+//recipeList.textContent
     //Appending the dynamically generated html to the div associated with the id="users"
     //Append will attach the element as the bottom most child.
-    document.getElementById('link').append(userName, userUrl)
+    document.getElementById('recipe').append(userName, userUrl)
     
-  // }
+   }
 });
-}
+ }
+
